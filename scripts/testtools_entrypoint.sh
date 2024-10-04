a8e1436f9c9096ffe2fac6174a6ca404097ba238
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2024-10-04 15:10:25 +0100

    feat: Minor changes to docker scripts to enable status reporting
    Also added a panel to download the api calls csv

--- scripts/testtools_entrypoint.sh
+++ scripts/testtools_entrypoint.sh
@@ -5,5 +5,6 @@ while true; do
     sleep 300
     echo "Processing API calls..."
     python /usr/local/bin/process_api_calls.py /var/www/html/tcpdump_output/api_calls.pcap /logs/api_calls.csv
+    cp /logs/api_calls.csv /var/www/html/api_calls.csv
     echo "API calls processed and saved to /logs/api_calls.csv"
 done
