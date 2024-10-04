0d4f65e44c5204902ffd54bd9de11272b50aff4f
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2024-10-04 16:40:03 +0100

    Updated paths to the logs and made the csv available to download

--- scripts/monitor_api_calls.sh
+++ scripts/monitor_api_calls.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Directory to store tcpdump output
-TCPDUMP_DIR="/var/www/html/tcpdump_output"
+TCPDUMP_DIR="/var/www/html/logs/tcpdump_output"
 mkdir -p "$TCPDUMP_DIR"
 
 # Start tcpdump to capture HTTP traffic
+echo "Starting tcpdump to capture API calls..."
 tcpdump -i eth0 -w "$TCPDUMP_DIR/api_calls.pcap" -s0 port 80 or port 443 &
 
 # Keep the script running
--- scripts/testtools_entrypoint.sh
+++ scripts/testtools_entrypoint.sh
@@ -2,9 +2,8 @@
 
 # Process the captured data every 5 minutes
 while true; do
-    sleep 300
+    sleep 60
     echo "Processing API calls..."
-    python /usr/local/bin/process_api_calls.py /var/www/html/tcpdump_output/api_calls.pcap /logs/api_calls.csv
-    cp /logs/api_calls.csv /var/www/html/api_calls.csv
+    python /usr/local/bin/process_api_calls.py /logs/tcpdump_output/api_calls.pcap /logs/api_calls.csv
     echo "API calls processed and saved to /logs/api_calls.csv"
 done
